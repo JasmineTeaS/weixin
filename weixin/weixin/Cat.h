cbd187f096f01f27c1d9f38484b321575a6d8286
253770337@qq.com <253770337@qq.com>
2017-02-27 07:27:13 +0800

    创建Cat类

---
+++ weixin/weixin/Cat.h
@@ -0,0 +1,13 @@
+//
+//  Cat.h
+//  weixin
+//
+//  Created by 冯 on 17/2/27.
+//  Copyright © 2017年 BEIJING JINGHUAHUADIAN DIGITAL TECHNOLOGY CO.,LTD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Cat : NSObject
+
+@end
---
+++ weixin/weixin/Cat.m
@@ -0,0 +1,13 @@
+//
+//  Cat.m
+//  weixin
+//
+//  Created by 冯 on 17/2/27.
+//  Copyright © 2017年 BEIJING JINGHUAHUADIAN DIGITAL TECHNOLOGY CO.,LTD. All rights reserved.
+//
+
+#import "Cat.h"
+
+@implementation Cat
+
+@end
